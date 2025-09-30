2b9ade12ecb1386c58b8123a8bf93f7d230d1d63
felix5572 <yfb222333@gmail.com>
2025-09-29 23:10:08 -0500

    fix: update model configuration in deepmd_agent to use deepseek-chat instead of gemini-2.5-flash

--- deepmd_ai_services/deepmd_workbench/deepmd_agent_services/deepmd_agent/agent.py
+++ deepmd_ai_services/deepmd_workbench/deepmd_agent_services/deepmd_agent/agent.py
@@ -22,9 +22,13 @@ load_dotenv()
 
 litellm.use_litellm_proxy = True
 
-model_original = "gemini-2.5-flash"
+# model_original = "gemini-2.5-flash"
 
-general_model = LiteLlm(model="gemini/gemini-2.5-flash"
+# general_model = LiteLlm(model="gemini/gemini-2.5-flash"
+#     , api_base=os.getenv("LITELLM_PROXY_API_BASE"),
+#     api_key=os.getenv("LITELLM_PROXY_API_KEY"),
+#     )
+general_model = LiteLlm(model="gemini/deepseek-chat"
     , api_base=os.getenv("LITELLM_PROXY_API_BASE"),
     api_key=os.getenv("LITELLM_PROXY_API_KEY"),
     )
